2e403cd30c43e50160d0dbee517b26bebf2373cd
Addd12 <54755940+Addd12@users.noreply.github.com>
2021-04-18 02:20:13 +0300

    register, login, logout, profile

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path("profile/", profile, name='profile')
+    #path("profile/", profile, name='profile')
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('profile/', profile, name='profile')
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,53 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm, ProfileForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            print('======user registered============')
+        
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+        profile_form = ProfileForm()
+    context = {'form': form, 'profile_form': profile_form}
+    return render(request, 'register.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
 def profile(request):
     return render(request, 'profile.html')
